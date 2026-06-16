bad9830bb05ea99fc39ef00f14adada6b576ddc7
RodrigoVcc <cc22070@ues.edu.sv>
2026-06-15 23:18:57 -0600

    9

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.PORT) || 3000,
+    port: Number(process.env.PORT) || 5173,
     allowedHosts: ['ifn135.onrender.com'],
     proxy: {
       '/api': {
